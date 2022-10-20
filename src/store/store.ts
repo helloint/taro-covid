d4148f0c896211058aaca532cf053b752de8e459
Wayne Mao <helloint@qq.com>
2022-10-20 17:39:22 +0800

    fix weird exception
    only happened on webapp prod

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,17 +6,9 @@ const reducer = {
   dailyTotal: dailyTotalReducer,
 };
 
-// TODO: how it works with RTK? `composeWithDevTools` is already handled by configureStore,
-//  the problem is just for the `options`
-// const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-//   ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-//     // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-//   })
-//   : compose
-
 const store = configureStore({
   reducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat([process.env.NODE_ENV === 'development' ? logger : null]),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   devTools: process.env.NODE_ENV !== 'production',
 });
 
